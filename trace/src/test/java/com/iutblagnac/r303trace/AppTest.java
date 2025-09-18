3cae88e55dd064b0e6ceb3169a354704fc2da4ab
Jonah PASCAL <jonah.pascal@orange.fr>
2025-09-18 12:01:05 +0200

    Nettoyage AppTest.java

--- trace/src/test/java/com/iutblagnac/r303trace/AppTest.java
+++ trace/src/test/java/com/iutblagnac/r303trace/AppTest.java
@@ -19,27 +19,5 @@ public class AppTest
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Test the return with empty parameter
-     */
-    public void testHelloEmptyParameter()
-    {
-        assertEquals("Hello World!", App.hello());
-    }
-
-    /**
-     * Test the return with a parameter
-     */
-    public void testHelloParameter()
-    {
-        assertEquals("Hello JMB!", App.hello("Hello JMB!"));
-    }
 }
